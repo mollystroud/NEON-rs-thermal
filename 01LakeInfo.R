2eb35d50f3ee5e7cdb3ce8ba77c2214d194b4e7f
mollystroud <mollykstroud@gmail.com>
2026-02-02 11:11:06 -0500

    Organized

--- 01LakeInfo.R
+++ 01LakeInfo.R
@@ -2,7 +2,7 @@
 # Author: Molly Stroud
 # Started 1/29/26
 ################################################################################
-
+pacman::p_load(sf)
 # Input below:
 # 1. Your desired bounding box coordinates (and UTM zone)
 # 2. The coordinates of a representative point(s) over your lake or reservoir of interest
@@ -14,31 +14,34 @@
 
 # a four letter site name
 # EXAMPLE: SUGG for Lake Suggs
-site <- "SUGG"
+site <- "sres"
 # specify bounding box
-bbox <- c(xmin = -82.0214,
-          ymin = 29.6843,
-          xmax = -82.0142,
-          ymax = 29.6912)
+bbox <- c(xmin = -71.1222,
+          ymin = 42.4402,
+          xmax = -71.1085,
+          ymax = 42.4503)
 
 # input UTM zone (necessary for accessing remote sensing data)
 # check your UTM zone: https://mangomap.com/robertyoung/maps/69585/what-utm-zone-am-i-in-#
-# get your EPSG: https://epsg.io/
-EPSG <- 32617
+utm <- 19 # for zone 19
+EPSG <- 32600 + utm
 box_utm <- sf::st_bbox(
   sf::st_transform(sf::st_as_sfc(sf::st_bbox(bbox,crs = "EPSG:4326")), paste0("EPSG:", EPSG)))
 
-# representative point(s) of water bodies
-points_df <- data.frame(lon = c(-82.018), lat = c(29.688))
+# pick representative point(s) of lake
+# for example, if your lake is a perfect circle, a good point would be the
+# middle of the circle
+points_df <- data.frame(lon = c(-71.1165), lat = c(42.4448))
 points <- st_as_sf(x = points_df,
                    coords = c("lon", "lat"),
                    crs = 4326)
 points <- sf::st_transform(points, crs = EPSG)
 
 
-# dates of interest 
+# dates over which you want to run forecasts
 # **START DATE MUST BE AFTER 2020-10-01**
-start_date <- "2025-07-01"
-end_date <- "2025-07-05"
+start_date <- "2025-10-01"
+end_date <- "2025-10-10"
+#end_date <- "2025-11-01"
 
 
--- 02Get_Inputs.R
+++ 02Get_Inputs.R
@@ -8,8 +8,8 @@
 # 2. Download meteorological data
 # 3. Grab bathymetry data (OPTIIONAL)
 # 4. Grab Kw factor
-# 5. Create GLM file
-# 6. Run FLARE
+# 5. Eestimate sediment zone info
+# 6. Create GLM file
 
 # get lake specifications
 source("01LakeInfo.R")
@@ -20,12 +20,12 @@ source("01LakeInfo.R")
 # or over a large lake, this will take a long time. Consider looping through
 # chunks of time in this section and merging the dfs together at the end
 ################################################################################
-
 source("get_LST.R")
 data <- get_lst(bbox, 
         box_utm, 
         paste0(start_date, "T00:00:00Z"), 
         paste0(end_date, "T00:00:00Z"))
+# now mask out non-water pixels
 masked_data <- water_mask(data)
 # see what it looks like!
 ggplot() +
@@ -33,17 +33,20 @@ ggplot() +
   facet_wrap(~time) +
   theme_classic() +
   scale_fill_viridis(na.value = 'transparent') +
-  labs(fill = "Temperature (C)")
-
+  labs(fill = "Temperature (C)") +
+  coord_fixed()
+# get values
 thermal_vals <- get_vals(points, masked_data)
 output <- clean_data(thermal_vals)
 # save out targets
-write_csv(output, paste0('targets/', site, '/', site, '-targets-rs-test.csv'))
+# create directory for targets file
+dir.create(paste0('targets/', site, '/'))
+write_csv(output, paste0('targets/', site, '/', site, '-targets-rs.csv'))
 
 
 ################################################################################
 # 2. Download meteorological data
-# Warning: this will take a long time
+# Warning: this may take a while depending on length of your date range
 # Warning: python must be installed to run this 
 ################################################################################
 source("get_met.R")
@@ -52,9 +55,11 @@ get_stage_2(start_date, end_date, site, bbox)
 # download stage 3
 get_stage_3(start_date, site, bbox)
 
+
 ################################################################################
 # 3. Get bathymetric data (OPTIONAL!)
-# If you already have existing bathymetry, skip to line 101
+# If you already have existing bathymetry, skip to get_ha function and input your
+# bathymetry raster
 ################################################################################
 # get bathymetry from GLOBathy
 source("get_bathy.R")
@@ -76,7 +81,7 @@ mylake_kw <- get_kw_global(bbox)
 
 # If your lake was unavailable in either of these databases, 
 # you may set mylake_kw based on knowledge of your lake of interest
-# If your lake is very turbid (Secchi < ):
+# If your lake is very turbid (Secchi < 1):
 # mylake_kw <- 1.7/1
 # If your lake is somewhere between turbid and clear (Secchi > 1, < 5):
 # mylake_kw <- 1.7/3
@@ -85,13 +90,22 @@ mylake_kw <- get_kw_global(bbox)
 
 
 ################################################################################
-# 5. Create GLM file
+# 5. Estimate sediment zone info
 ################################################################################
-
+source("get_SedZoneInfo.R")
+# first get air temperature data over a few years
+era5_download <- get_historical_weather(latitude = points_df$lat,
+                                        longitude = points_df$lon,
+                                        start_date = Sys.Date() - 2000, # get a long enough date range
+                                        end_date = Sys.Date(),
+                                        variables = c("temperature_2m"))
+# the average sediment zone temperature is comparable to average air temperature
+sed_temp <- mean(era5_download$prediction)
+# if the lake is < 5 m deep, the peak doy and amplitude are also comparable to air temp
+sed_data <- get_sed_zone_data(era5_download, (max(values(bathy), na.rm = T) - min(values(bathy), na.rm = T)))
 
 
 ################################################################################
-# 6. FLARE! (Open up combined_run.R)
+# 6. Create GLM file
 ################################################################################
-
 
