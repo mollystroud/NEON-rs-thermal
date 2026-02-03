af49654064da47f48c06c1a99ea9912e8e8939cd
mollystroud <mollykstroud@gmail.com>
2026-02-03 10:23:56 -0500

    Added in GLM nml file editing

--- 02Get_Inputs.R
+++ 02Get_Inputs.R
@@ -66,7 +66,7 @@ get_stage_3(start_date, site, bbox)
 source("get_bathy.R")
 bathy <- find_matches(bbox)
 plot(bathy) # check this is the correct lake
-get_ha(bathy, points)
+ha <- get_ha(bathy, points)
 
 ################################################################################
 # 4. Get Kw factor (light extinction)
@@ -107,6 +107,27 @@ sed_data <- get_sed_zone_data(era5_download, (max(values(bathy), na.rm = T) - mi
 
 
 ################################################################################
-# 6. Create GLM file
+# 6. Create GLM file with proper variables
 ################################################################################
+source("edit_nml_functions.R")
+remotes::install_github('usgs-r/glmtools', force = T, upgrade = 'never')
+library(glmtools)
+
+var_list <- list(site, mylake_kw, site, points_df[[2]], points_df[[1]],
+                 dim(ha)[1], rev(ha$depths), rev(ha$Area.at.z),
+                 (max(ha$depths) - min(ha$depths)), 
+                 sed_temp, c(sed_data[1], sed_data[2]), c(sed_data[3], sed_data[4]))
+var_name_list <- list("sim_name", "Kw", "lake_name", "latitude", "longitude",
+                      "bsn_vals", "H", "A", "lake_depth",
+                      "sed_temp_mean", "sed_temp_amplitude", "sed_temp_peak_doy")
+# create list of variable values & names for input to nml
+update_nml(var_list, var_name_list, 
+           working_directory = 'configuration/analysis', nml = 'glm3_base.nml')
+
+
+
+
+
+
+
 
