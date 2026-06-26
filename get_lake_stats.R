00b4e9499076876ef164c5068285ebc2581bbc52
mollystroud <mollykstroud@gmail.com>
2026-06-26 11:28:01 -0400

    lake stats code

--- get_lake_stats.R
+++ get_lake_stats.R
@@ -86,6 +86,45 @@ rs <- read_csv('/Users/mollystroud/Desktop/postdoc/flare-rs-thermal/targets/fcre
 
 
 # wind speed
+# for NEON sites
+product <- "DP1.20059.001" # wind speed
+sites <- c("PRLA", "PRPO", "SUGG", "LIRO", "CRAM", "BARC") # all sites (takes longer)
+
+# download available files
+neonstore::neon_download(product = product, site = sites, 
+                         start_date = "2021-01-01",
+                         end_date = "2023-01-01")
+wind <- neonUtilities::stackFromStore(filepaths=tools::R_user_dir("neonstore"),
+                               dpID="DP1.20059.001",
+                               package="basic",
+                               site=sites)
+wind_30min <- wind$WSDBuoy_30min |>
+  group_by(siteID) |>
+  summarize(mean_wind = mean(buoyWindSpeedMean, na.rm = T))
+# for wvwa sites
+carvins <- read_csv('https://pasta.lternet.edu/package/data/eml/edi/1105/4/8ebf27393ccafe518328468a260d2e18') |>
+  filter(DateTime >= "2021-01-01") |>
+  filter(DateTime <= "2023-01-01") |>
+  summarize(mean_wind = mean(WindSpeed_Average_m_s, na.rm = T))
+
+fcre_bvre <- read_csv('https://pasta.lternet.edu/package/data/eml/edi/389/10/d3f3d2fa40c41fdcd505ae49b2fdcf8b') |>
+  filter(DateTime >= "2021-01-01") |>
+  filter(DateTime <= "2023-01-01") |>
+  summarize(mean_wind = mean(WindSpeed_Average_m_s, na.rm = T))
+# for sunp
+# mean annual air temperature
+devtools::install_github("FLARE-forecast/ropenmeteo", force = T, upgrade = "never")
+library(ropenmeteo)
+library(zoo)
+
+
+era5_download <- get_historical_weather(latitude = 43.383904,
+                                        longitude = -72.054173,
+                                        start_date = "2021-01-01",
+                                        end_date = "2023-01-01",
+                                        variables = c("wind_speed_10m")) # precipitation
+
+mean(era5_download$prediction)
 
 
 
@@ -98,28 +137,29 @@ library(rLakeAnalyzer)
 
 sitenames <- list.files('targets')
 for(site in sitenames){
-  data <- read_csv(paste0('targets/', site, '/', site, '-targets-insitu.csv')) |>
-    filter(datetime >= "2020-12-01") |>
+  data <- read_csv(paste0('targets/', site, '/', site, '-targets-insitu.csv'), show_col_types = F) |>
+    filter(datetime >= "2020-12-31") |>
     filter(datetime <= "2023-01-01") |>
+    filter(variable == 'temperature') |>
     filter(!is.na(observation)) |>
     select(-c(variable, site_id))
   mindepth <- sort(unique(data$depth))[2]
-  maxdepth <- tail(sort(unique(data$depth)), n = 2)[1]
+  #maxdepth <- tail(sort(unique(data$depth)), n = 2)[1]
+  maxdepth <- max(sort(unique(data$depth)))
   data_wide <- data |>
     filter(depth %in% c(mindepth, maxdepth)) |>
     pivot_wider(names_from = depth,
                 values_from = observation) |>
     rename_with(~c('datetime', 'temp_shallow', 'temp_deep')) |>
+    drop_na() |>
     mutate(diff_c = temp_shallow - temp_deep,
            diff_dens = water.density(temp_deep) - water.density(temp_shallow),
            strat_temp = ifelse(diff_c < 1, "Not_Strat", "Strat"),
            strat_dens = ifelse(diff_dens > 0.1, "Strat", "Not_Strat"))
   days_strat <- sum(data_wide$strat_dens == "Strat")
-  print(days_strat/2) # AND NORMALIZE BY n DAYS
+  print(paste0(site, ' ', days_strat/2)) # AND NORMALIZE BY n DAYS
   
 }
-
-
 
 
 
